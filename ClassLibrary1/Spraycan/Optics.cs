0fdbf5554b88c417034363f228444e89464a38e3
Prof. Dru <dball89@optonline.net>
2020-10-30 15:07:39 -0400

    Find spectator camera and enable it

--- ClassLibrary1/Spraycan/Optics.cs
+++ ClassLibrary1/Spraycan/Optics.cs
@@ -12,9 +12,11 @@ namespace BreakdanceBeach.Spraycan
 	/// </summary>
 	static class Optics
 	{
+		static Camera spec_camera;
 		public static void SessionStart()
 		{
 			DestroySpectatorHud();
+			SetSpectatorCamera();
 		}
 
 		/// <summary>
@@ -25,6 +27,32 @@ namespace BreakdanceBeach.Spraycan
 			var spec_hud = GameObject.Find("Canvas_SpectatorHUD");
 			spec_hud.SetActive(false);
 		}
+		public static void SetSpectatorCamera()
+		{
+			var spec_cam_root = GameObject.Find("CameraHolder1");
+			if (!spec_cam_root)
+			{
+				Debug.LogWarning("Spectator camera root couldn't be found");
+				return;
+			}
 
+			var spec_container = spec_cam_root.transform.Find("SpectatorCam_VR").gameObject;
+			if (!spec_container)
+			{
+				Debug.LogWarning("Spectator container couldn't be found.");
+				return;
+			}
+			spec_container.SetActive(true);
+
+			spec_camera = spec_container.GetComponent<Camera>();
+
+			if (!spec_camera)
+			{
+				Debug.LogWarning("Spectator container couldn't be found.");
+				return;
+			}
+			spec_camera.fieldOfView = 120f;
+			spec_camera.cullingMask = spec_camera.cullingMask | 31 | 35 | 10;
+		}
 	}
 }
