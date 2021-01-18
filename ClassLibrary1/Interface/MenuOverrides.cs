e35f5f6247a2f3279617c85d58b0c829132d33b3
Prof. Dru <dball89@optonline.net>
2021-01-18 15:53:40 -0500

    Add unique queries for each social board button

--- ClassLibrary1/Interface/MenuOverrides.cs
+++ ClassLibrary1/Interface/MenuOverrides.cs
@@ -3,22 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using HarmonyLib;
+using BreakdanceBeach.Interface;
 
 namespace BreakdanceBeach.Interface
 {
 	class MenuOverrides
 	{
-
 		/// <summary>
-		/// 
 		/// 
 		/// </summary>
 		[HarmonyPatch]
 		[HarmonyPatch(typeof(SongSelector_SongPane), "BuildList_FromSocialBoard")]
 		public class Overrider
 		{
-			const string GetRandomSongQuery = "SELECT * FROM songs ORDER BY random() LIMIT 25";
-			const string GetRecentlyAddedSongQuery = "SELECT * FROM songs ORDER BY songs.songid DESC LIMIT 25";
+			const string GetRecentlyAddedSongQuery = "SELECT * FROM songs ORDER BY songs.songid DESC LIMIT 24";
+			const string GetRandomUnplayedSongQuery = "SELECT * FROM songs WHERE songs.lastplaytime IS NULL ORDER BY random() LIMIT 24";
+			const string GetNewestUnplayedQuery = "SELECT * FROM songs WHERE songs.lastplaytime IS NULL ORDER BY songs.songid DESC LIMIT 24";
+
+			static Dictionary<SocialState, string> state_to_query = new Dictionary<SocialState, string>()
+			{
+				{ SocialState.POPULAR_MY_FRIENDS, GetRecentlyAddedSongQuery },
+				{ SocialState.SAMPLE_SONGS, GetNewestUnplayedQuery},
+				{ SocialState.POPULAR_OTHER_GAME, GetRandomUnplayedSongQuery},
+			};
 
 			public static bool Prefix(
 				SongSelector_SongPane __instance,
@@ -26,21 +33,19 @@ namespace BreakdanceBeach.Interface
 				bool streamingCheck = false
 			)
 			{
-				BreakdanceBeach.Database.DBRedirector.SetNextDBString(GetRecentlyAddedSongQuery);
+				// Change the query based on the tracker state. This tells us what menu was clicked
+				// on before this function was called
+				string query = GetRandomUnplayedSongQuery;
+				var tracker_state = SongGridTracker.GetSocialState();
+				if (state_to_query.ContainsKey(tracker_state))
+					query = state_to_query[tracker_state];
 
+				// Set the next query to this, then load the songs
+				BreakdanceBeach.Database.DBRedirector.SetNextDBString(query);
 				var songs = ASDatabase.instance.GetFavoriteSongs(10, true);
 				__instance.StartCoroutine(ReverseMethods.DisplaySongs(__instance, songs));
 				return false;
 			}
-
-
-			public void LoadRecentlyAddedSongs()
-			{
-
-			}
-
 		}
-
-
 	}
 }
---
+++ ClassLibrary1/Interface/SongGridTracker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+
+namespace BreakdanceBeach.Interface
+{
+	public enum SocialState : int
+	{
+		SAMPLE_SONGS = 0,
+		POPULAR_OTHER_GAME = 1,
+		POPULAR_MY_FRIENDS = 2
+	}
+
+	/// <summary>
+	/// Keeps track of which category was selected last in the social boards
+	/// </summary>
+	[HarmonyPatch]
+	[HarmonyPatch(typeof(SongSelector_SongPane), "OnSongGrid_Show")]
+	public class SongGridTracker
+	{
+		private static Dictionary<String, SocialState> state_translator = new Dictionary<string, SocialState>(){
+			{"samplesongs", SocialState.SAMPLE_SONGS},
+			{"popularothergame", SocialState.POPULAR_OTHER_GAME},
+			{"popularwithfriends", SocialState.POPULAR_MY_FRIENDS}
+		};
+
+		private static SocialState last_state = SocialState.SAMPLE_SONGS;
+
+		/// <summary>
+		/// Get the social state of the last clicked on menu item 
+		/// </summary>
+		public static SocialState GetSocialState()
+		{
+			return last_state;
+		}
+
+		/// <summary>
+		/// Before running this function, log the last social state 
+		/// </summary>
+		public static bool Prefix(string showCategory)
+		{
+			UnityEngine.Debug.Log("Updating tracker state.");
+
+			if (state_translator.ContainsKey(showCategory))
+				last_state = state_translator[showCategory];
+
+			return true;
+		}
+	}
+}
